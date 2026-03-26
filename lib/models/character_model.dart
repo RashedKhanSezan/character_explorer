a05b0ea73d4b5d980045601c6af609441e4fd046
RashedKhanSezan <rashedkhansezan@gmail.com>
2026-03-26 23:03:36 +0600

    updated the model

--- lib/models/character_model.dart
+++ lib/models/character_model.dart
@@ -1,3 +1,45 @@
+enum Gender { male, female, genderless, unknown }
+
+enum Status { alive, dead, unknown }
+
 class CharacterModel {
-  
+  final int id;
+  final String name;
+  final Gender gender;
+  final String species;
+  final Status status;
+  final String type;
+  final String imagePath;
+  final Map<String, dynamic> origin;
+  final Map<String, dynamic> location;
+
+  CharacterModel({
+    required this.id,
+    required this.name,
+    required this.gender,
+    required this.species,
+    required this.status,
+    required this.type,
+    required this.imagePath,
+    required this.origin,
+    required this.location,
+  });
+
+  factory CharacterModel.fromJson(Map<String, dynamic> json) {
+    return CharacterModel(
+      id: json['id'],
+      name: json['name'],
+      gender: Gender.values.firstWhere((element) {
+        return element.name == (json['gender'] as String).toLowerCase();
+      }, orElse: () => Gender.unknown),
+      species: json['species'],
+      status: Status.values.firstWhere((element) {
+        return element.name == (json['status'] as String).toLowerCase();
+      }, orElse: () => Status.unknown),
+      type: json['type'],
+      imagePath: json['image'],
+      origin: Map<String, dynamic>.from(json['origin']),
+      location: Map<String, dynamic>.from(json['location']),
+    );
+  }
 }
