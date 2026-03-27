c69e6dced09341a2a4ccaac6ef5966ed36754747
RashedKhanSezan <rashedkhansezan@gmail.com>
2026-03-27 14:49:10 +0600

    moviing to Dio form Http

--- lib/controllers/character_controller.dart
+++ lib/controllers/character_controller.dart
@@ -1,36 +1,29 @@
-import 'dart:convert';
-
-import 'package:character_explorer/models/character_model.dart';
 import 'package:get/get.dart';
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
+import 'package:character_explorer/models/character_model.dart';
 
 class CharacterController extends GetxController {
   var charlist = <CharacterModel>[].obs;
   var isLoading = true.obs;
-
-  @override
-  void onInit() {
-    fetchData();
-    super.onInit();
-  }
+  final Dio _dio = Dio();
 
   void fetchData() async {
     try {
       isLoading(true);
-      final response = await http.get(
-        Uri.parse('https://rickandmortyapi.com/api/character'),
+      final response = await _dio.get(
+        'https://rickandmortyapi.com/api/character',
       );
 
       if (response.statusCode == 200) {
-        final charData = jsonDecode(response.body);
-
+        var charData = response.data;
         var allChar = CharacterData.fromJson(charData);
-        charlist.addAll(allChar.allCharacter);
+
+        charlist.assignAll(allChar.allCharacter);
       }
-    } on Exception catch (e) {
-      Get.snackbar('Something', '$e');
-    } finally {
-      isLoading(false);
+    } on DioException catch (e) {
+      String errormsg = e.response!.data['error'] ?? "Some error";
+
+      Get.snackbar('Somthing Wrong', errormsg);
     }
   }
 }
